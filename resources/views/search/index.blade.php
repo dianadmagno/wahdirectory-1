dbc391439297dd6d4866206b0823adc85b425a09
Diana Magno <dianamagno20@gmail.com>
2017-06-19 08:29:52 +0800

    Home

---
+++ resources/views/search/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+/*<form>
+    <div class="input-group"> 
+        <span class = "black-text text-darken-2" ><h4 >
+        <input type="text" class="form-control bordered-highlight" name="q" 
+            placeholder="Search"> <span class="input-group-btn" style="width:200px;padding:20px"></span>
+             <button type="submit" class="btn btn-default light-blue lighten-1 material-icons">search
+                <span class="glyphicon glyphicon-search" ></span></h4>
+            </button>
+        </span>
+    </div>
+</form>
+@endsection
